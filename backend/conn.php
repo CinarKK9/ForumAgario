b2e4ecf87a0ee7d27441fa571b1ba763c53a39d0
CinarKK9 <cinarkk9@gmail.com>
2023-08-19 11:13:15 +0300

    added sign up funticon

--- backend/conn.php
+++ backend/conn.php
@@ -2,8 +2,4 @@
     function connect() {
         $conn = new mysqli('hehehehehe_server_name', 'hehehehehe_username', 'hehehehehe_password', 'hehehehehe_db_name');
     }
-
-    function disconnect() {
-        $conn->close();
-    }
 ?>
--- backend/signup.php
+++ backend/signup.php
@@ -2,4 +2,33 @@
     include './conn.php';
 
     connect();
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["usermail"];
+    $password = $_POST["userpass"];
+
+    $uname_query = "SELECT id FROM users WHERE mail = ?";
+    $uname_stmt = $conn->prepare($uname_query);
+    $uname_stmt->bind_param("s", $username);
+    $uname_stmt->execute();
+    $check_result = $uname_stmt->get_result();
+
+    if ($check_result->num_rows > 0) {
+        echo "This email is already in use. Please login.";
+    } else {
+        $signup_query = "INSERT INTO users (username, password) VALUES (?, ?)";
+        $signup_stmt = $conn->prepare($signup_query);
+        $signup_stmt->bind_param("ss", $username, $password);
+        
+        if ($signup_stmt->execute()) {
+            echo "You successfully signed up.";
+        } else {
+            echo "Something went wrong.";
+        }
+    }
+
+    $uname_stmt->close();
+    $signup_stmt->close();
+    $conn->close();
+}
 ?>
